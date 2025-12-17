d5592ad0e62af764f84ee72356ae7e80e425f129
Twicefear <ytd82774@gmail.com>
2025-12-17 09:56:42 +0530

    Update SimpleCIT.java

--- src/main/java/com/example/simplecit/SimpleCIT.java
+++ src/main/java/com/example/simplecit/SimpleCIT.java
@@ -1,1 +1,13 @@
+package com.example.simplecit;
 
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.minecraft.resource.ResourceType;
+
+public class SimpleCIT implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES)
+                .registerReloadListener(CITResourceReloader.INSTANCE);
+    }
+}
