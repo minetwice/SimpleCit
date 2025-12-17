5f3a6379f3077139982663055ddfcf3292571107
Twicefear <ytd82774@gmail.com>
2025-12-17 09:58:44 +0530

    Update CITResourceReloader.java

--- src/main/java/com/example/simplecit/CITResourceReloader.java
+++ src/main/java/com/example/simplecit/CITResourceReloader.java
@@ -1,1 +1,39 @@
+package com.example.simplecit;
 
+import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.util.Identifier;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class CITResourceReloader implements SimpleSynchronousResourceReloadListener {
+    public static final CITResourceReloader INSTANCE = new CITResourceReloader();
+
+    @Override
+    public Identifier getFabricId() {
+        return Identifier.of("simplecit", "cit_reloader");
+    }
+
+    @Override
+    public void apply(ResourceManager manager) {
+        CITManager.clearRules();
+
+        manager.findResources("optifine/cit", id -> id.getPath().endsWith(".properties"))
+                .forEach((id, resourceRef) -> {
+                    try (InputStream is = resourceRef.getReader().read()) {
+                        Properties props = new Properties();
+                        props.load(is);
+
+                        String namespace = id.getNamespace();
+                        CITRule rule = CITRule.fromProperties(props, namespace);
+                        if (rule != null) {
+                            CITManager.addRule(rule);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+    }
+}
