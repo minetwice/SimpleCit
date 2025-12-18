a303f1665d742b625dbc991e074f7b9e67f7d5a4
Twicefear <ytd82774@gmail.com>
2025-12-18 12:52:15 +0530

    Update SimpleCitMod.java

--- src/main/java/com/example/simplecit/SimpleCitMod.java
+++ src/main/java/com/example/simplecit/SimpleCitMod.java
@@ -3,11 +3,17 @@ package com.example.simplecit;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.minecraft.resource.ResourceType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class SimpleCitMod implements ModInitializer {
+public class SimpleCIT implements ModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("simplecit");
+
     @Override
     public void onInitialize() {
+        LOGGER.info("SimpleCIT Initializing...");
         ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES)
             .registerReloadListener(CITResourceReloader.INSTANCE);
+        LOGGER.info("SimpleCIT Initialized!");
     }
 }
